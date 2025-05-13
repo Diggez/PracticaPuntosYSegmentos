90ce9caf571a8427a67bfc890eebfb9c8e7dfc99
Diggez <danielvieico1234@gmail.com>
2025-05-13 16:59:59 +0200

    Javadoc

--- PracticaPuntosYSegmentos/src/clases/Punto.java
+++ PracticaPuntosYSegmentos/src/clases/Punto.java
@@ -2,4 +2,46 @@ package clases;
 
 public class Punto {
 
+	private int x;
+	private int y;
+	
+	/**
+	 *Crea un Punto en la posicion (0,0)
+	 */
+	public Punto () {
+		this.x=0;
+		this.y=0;
+	}
+	/**
+	 * Crea un Punto en una posicion concreta
+	 * @param x
+	 * @param y
+	 */
+	public Punto(int x, int y) {
+		this.x=x;
+		this.y=y;
+	}
+	
+	public int getX() {
+	return this.x;	
+	}
+	
+	public int getY() {
+	return this.y;
+	}
+	
+	public void setX(int nX) {
+		this.x=nX;
+	}
+	
+	public void setY(int nY) {
+		this.y=nY;
+	}
+	
+	@Override
+	
+	public String toString() {
+		String mensaje="("+this.x+","+this.y+")";
+	return mensaje;
+	}
 }
--- PracticaPuntosYSegmentos/src/clases/Segmento.java
+++ PracticaPuntosYSegmentos/src/clases/Segmento.java
@@ -2,4 +2,55 @@ package clases;
 
 public class Segmento {
 
+	private Punto a;
+	private Punto b;
+	
+	/**
+	 * Crea un Segmento de dos Punto en las posiciones 0,0 y 1,1
+	 */
+	public Segmento() {
+		a=new Punto(0,0);
+		b=new Punto(1,1);
+	}
+	/**
+	 * Crea un Segmento de dos puntos especificos
+	 * @param a
+	 * @param b
+	 */
+	public Segmento(Punto a, Punto b) {
+		
+		this.a=a;
+		this.b=b;
+		
+	}
+	
+	public Punto getA() {
+	return a;	
+	}
+	public Punto getB() {
+	return b;	
+	}
+	
+	public void setA(Punto nA) {
+		a=nA;
+	}
+	
+	public void setB(Punto nB) {
+		b=nB;
+	}
+	/**
+	 * Calcular la distancia entre los dos Puntos del Segmento
+	 * @return la distancia entre los dos Puntos del Segmento
+	 */
+	public int distancia() {
+    int distancia=(int)Math.sqrt(Math.pow(a.getY()-a.getX(),2)+Math.pow(b.getY()-b.getX(),2));
+	return distancia;
+	}
+	
+	@Override
+	
+	public String toString() {
+	String mensaje="[("+a.getX()+","+a.getY()+")"+",("+b.getX()+","+b.getY()+")]";
+	return mensaje;
+	}
 }
